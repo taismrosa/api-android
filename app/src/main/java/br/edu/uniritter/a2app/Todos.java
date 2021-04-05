2783c2dd0cbcff1e723fb6b66329002bb6310a66
Taís Medeiros da Rosa <taismedeirosdarosa@gmail.com>
2021-04-05 01:27:38 -0300

    v0.1: add essentials activities

---
+++ app/src/main/java/br/edu/uniritter/a2app/Todos.java
@@ -0,0 +1,14 @@
+package br.edu.uniritter.a2app;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Todos extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_todos);
+    }
+}
